d2bbe2fec86e7eb55fe9c1f4457ca5917f5c10c8
Daniel Alvarez <daniel.alvarezl@usach.cl>
2020-03-06 00:50:05 -0300

    change seed

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -12,7 +12,17 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
+//        User::create([
+//            'name' => 'Admin',
+//            'email' => 'daniel.alvarezl@usach.cl',
+//            'email_verified_at' => now(),
+//            'password' => bcrypt(env('BLOG_PASS')),
+//            'admin' => 1,
+//            'approved_at' => now(),
+//        ]);
+        DB::table('users')->insert([
+//            'name' => 'title',
+//            'created_at' => date("Y-m-d H:i:s")
             'name' => 'Admin',
             'email' => 'daniel.alvarezl@usach.cl',
             'email_verified_at' => now(),
