aa06d231a2ed30e13bd569e6e475ce80d3f42754
Daniel Alvarez <daniel.alvarezl@usach.cl>
2020-03-17 22:43:32 -0300

    agregando orden a post

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
-    {   $posts = App\Post::all();
+    {   $posts = App\Post::orderBy('id','DESC')->get();
         $edit = false;
         return view('home',compact('posts','edit'));
     }
